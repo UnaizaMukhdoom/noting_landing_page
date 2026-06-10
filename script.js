1fd5463339d504345447514be70c7673a5cc085d
UnaizaMukhdoom <unaiza1mukhdoom@gmail.com>
2026-06-10 16:56:50 +0500

    remove the css

--- script.js
+++ script.js
@@ -29,16 +29,4 @@ form.addEventListener("submit", (e) => {
   message.textContent = "Form submitted successfully!";
 });
 
-const modal = document.getElementById("modal");
-const openModal = document.getElementById("openModal");
-const closeModal = document.getElementById("closeModal");
-
-openModal.addEventListener("click", () => {
-  modal.classList.add("show");
-});
-
-closeModal.addEventListener("click", () => {
-  modal.classList.remove("show");
-});
-
 
