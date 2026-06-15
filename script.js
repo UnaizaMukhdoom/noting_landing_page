bd27d9c24c04dabb7154197223bdc7a8de0a79b0
Unaiza Mukhdoom <unaiza1mukhdoom@gmail.com>
2026-06-15 11:24:25 +0500

    Update script.js

--- script.js
+++ script.js
@@ -168,8 +168,3 @@ closeModal.addEventListener("click", () => {
   modal.style.display = "none";
 });
 
-window.addEventListener("click", (e) => {
-  if (e.target === modal) {
-    modal.style.display = "none";
-  }
-});
